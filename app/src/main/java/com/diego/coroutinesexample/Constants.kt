3f655ff3968348c880dc21e9e0968e69f725f927
Diego Santos <diego.net10@hotmail.com>
2018-10-17 18:28:19 -0300

    Added unit tests

--- app/src/main/java/com/diego/coroutinesexample/Constants.kt
+++ app/src/main/java/com/diego/coroutinesexample/Constants.kt
@@ -3,5 +3,5 @@ package com.diego.coroutinesexample
 object Constants{
     val CLIENT_ID = "446965676f476f6ec3a7616c76657353616e746f737c376665326265326637313966356364373133643133663837"
     val CLIENT_PASSWORD  = "10ab648323c1d3ef8d5ccd530dbc38230c9caac35bd207515afd524666bb0cd4dea73f974260aee3d833f672faac0fe5"
-    val BASE_URL: String = "https://api.globalcode.com.br/v1/"
+    var BASE_URL: String = "https://api.globalcode.com.br/v1/"
 }
---
+++ app/src/test/java/com/diego/coroutinesexample/CoroutinesUnitTest.kt
@@ -0,0 +1,77 @@
+package com.diego.coroutinesexample
+
+import kotlinx.coroutines.experimental.runBlocking
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
+import java.io.File
+
+class CoroutinesUnitTest {
+
+    // Work around to user "any()" Matcher
+    // for not nullable parameters
+    // Casting null to the type required
+    private fun <T> any(): T {
+        Mockito.any<T>()
+        return uninitialized()
+    }
+    private fun <T> uninitialized(): T = null as T
+
+    @Mock lateinit var view: MainActivity
+    lateinit var presenter: EventsPresenter
+
+    lateinit var server: MockWebServer
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        presenter = EventsPresenter(view)
+
+        server = MockWebServer()
+        server.start()
+    }
+
+    @Test
+    fun getEventListTestSuccess() {
+        server.enqueue(MockResponse().setBody(getJson("json/token.json")))
+        server.enqueue(MockResponse().setBody(getJson("json/eventos.json")))
+
+        Constants.BASE_URL = server.url("/").toString()
+
+        runBlocking {
+            presenter.getEvents()
+        }
+
+        verify(view, times(1)).displayEvents(any())
+        verify(view, times(0)).showError()
+    }
+
+    @Test
+    fun getEventListTestError() {
+        server.enqueue(MockResponse().setResponseCode(400))
+
+        Constants.BASE_URL = server.url("/").toString()
+
+        runBlocking {
+            presenter.getEvents()
+        }
+
+        verify(view, times(0)).displayEvents(any())
+        verify(view, times(1)).showError()
+    }
+
+    fun getJson(path : String) : String {
+        // Load the JSON response
+        val uri = this.javaClass.classLoader.getResource(path)
+        val file = File(uri.path)
+        return String(file.readBytes())
+    }
+}
